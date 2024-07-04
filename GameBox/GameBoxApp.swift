c49cd8d18f8c718c7064ebb2fbc1f0b8e5fd5bfa
Guillaume Demerges <guillaume.demerges@protonmail.com>
2024-07-04 17:19:09 +0200

    Initial Commit

---
+++ GameBox/GameBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameBoxApp.swift
+//  GameBox
+//
+//  Created by Guillaume Demergès on 04/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct GameBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
